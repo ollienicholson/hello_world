61866391cb684b26f9abdf07f666ec2fb3db53fb
Oliver Nicholson <69487259+ollienicholson@users.noreply.github.com>
2023-03-27 09:58:52 +1300

    fds

--- Codewars/highest-lowest.py
+++ Codewars/highest-lowest.py
@@ -19,6 +19,7 @@ string1 = "1 2 -3 4 5"
 string2 = "1 9 3 4 10"
 
 
+# option 1 -submitted
 def high_and_low(numbers):
     numbers = numbers.split(' ')
     numbers = sorted(numbers, key=int)
@@ -26,3 +27,26 @@ def high_and_low(numbers):
 
 
 print(high_and_low(string))
+
+
+# option 2
+def high_and_low2(numbers):  #z.
+    nn = [int(s) for s in numbers.split(" ")]
+    return "%i %i" % (max(nn), min(nn))
+
+
+# option 3
+def high_and_low3(numbers):
+    nums = sorted(numbers.split(), key=int)
+    return '{} {}'.format(nums[-1], nums[0])
+
+
+#  option 4
+def high_and_low4(numbers):
+    numbers = [int(c) for c in numbers.split(' ')]
+    return f"{max(numbers)} {min(numbers)}"
+
+
+# option 5
+def high_and_low5(numbers):
+    return " ".join(x(numbers.split(), key=int) for x in (max, min))
